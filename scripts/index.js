8b914ae3fb1ea356aecb94e6a644018deba70e3a
MDaryaK <dasha-41@mail.ru>
2023-03-09 16:51:35 +0300

    переделанная работа

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,22 @@ const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__kusto');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.form__box_name');
+let userInput = formElement.querySelector('.form__box_user');
 let aboutInput = formElement.querySelector('.form__box_about');
-let nameInputNewValue = document.querySelector('.profile__subtitle');
+let userEnterNewValue = document.querySelector('.profile__subtitle');
 let aboutInputNewValue = document.querySelector('.profile__title');
 
 const openPopup = function () {
-  nameInput.value = nameInputNewValue.textContent;
+  userInput.value = userEnterNewValue.textContent;
   aboutInput.value = aboutInputNewValue.textContent;
   popupElement.classList.add('popup_opened');
 };
 
 const closePopup = function () {
   popupElement.classList.remove('popup_opened');
-
 };
 
-const closePopupByClickOverlay = function (event) {
+const closePopupStrap = function (event) {
   if (event.currentTarget === event.target) {
     closePopup();
   }
@@ -26,14 +25,14 @@ const closePopupByClickOverlay = function (event) {
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOverlay);
+popupElement.addEventListener('click', closePopupStrap);
 
-const handleFormSubmit = function (evt) {
+const processingFormSubmission = function (evt) {
   evt.preventDefault();
 
-  nameInputNewValue.textContent = nameInput.value;
+  userEnterNewValue.textContent = userInput.value;
   aboutInputNewValue.textContent = aboutInput.value;
   closePopup();
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', processingFormSubmission);
