fc2c8818b8e928ca9737e8ad20df964ef65625d3
MDaryaK <dasha-41@mail.ru>
2023-03-10 13:29:19 +0300

    изменения в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,25 @@
-const popupElement = document.querySelector('.popup');
-const popupOpenButtonElement = document.querySelector('.profile__kusto');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-const formElement = document.querySelector('.form');
-let userInput = formElement.querySelector('.form__box_user');
-let aboutInput = formElement.querySelector('.form__box_about');
-let userEnterNewValue = document.querySelector('.profile__subtitle');
-let aboutInputNewValue = document.querySelector('.profile__title');
+const openPopup = document.querySelector('.profile__kusto');
+const closePopup = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
 
-const openPopup = function () {
-  userInput.value = userEnterNewValue.textContent;
-  aboutInput.value = aboutInputNewValue.textContent;
-  popupElement.classList.add('popup_opened');
-};
+openPopup.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
+});
 
-const closePopup = function () {
-  popupElement.classList.remove('popup_opened');
-};
+closePopup.addEventListener('click', function closePopupButton() {
+  popup.classList.remove('popup_opened');
+});
 
-const closePopupStrap = function (event) {
-  if (event.currentTarget === event.target) {
-    closePopup();
-  }
-}
+ let nameInput = form.querySelector('.form__box_user');
+ let jobInput = form.querySelector('.form__box_about');
+ let nameNew = document.querySelector('.profile__subtitle');
+ let jobNew = document.querySelector('.profile__title');
+ let form = form.querySelector('.form');
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupStrap);
-
-const processingFormSubmission = function (evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
-
-  userEnterNewValue.textContent = userInput.value;
-  aboutInputNewValue.textContent = aboutInput.value;
-  closePopup();
+  nameInput.value = nameNew.textContent;
+  jobInput.value = jobNew.textContent;
+  closePopupButton();
 }
-
-formElement.addEventListener('submit', processingFormSubmission);
+form.addEventListener('submit', handleFormSubmit);
